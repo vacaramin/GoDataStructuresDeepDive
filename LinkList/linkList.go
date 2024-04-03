408dbd0388da33b629e73a549bc806e1f8737837
Waqar Amin <vacaramin86@gmail.com>
2024-04-04 02:50:18 +0500

    Link list init

--- LinkList/linkList.go
+++ LinkList/linkList.go
@@ -1,1 +1,24 @@
 package linklist
+
+type Node struct {
+	val  int
+	Next *Node
+}
+
+type LinkList struct {
+	head *Node
+}
+
+func (l *LinkList) Add(val int) {
+	newNode := Node{val: val, Next: nil}
+	if l.head != nil {
+		temp := l.head
+		for temp.Next != nil {
+			temp = temp.Next
+		}
+		temp.Next = &newNode
+	} else {
+		l.head = &newNode
+	}
+
+}
